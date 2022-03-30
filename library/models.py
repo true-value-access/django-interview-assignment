dc6781ed9dd9842b23b831587fc9358d04d69767
Sarkar Devashish <devashishsarkar54@gmail.com>
2022-03-31 01:33:59 +0530

    models created

--- library/models.py
+++ library/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+class MyUser(models.Model):
+    role = models.CharField(max_length=50, choices=[("LIBRARIAN", "LIBRARIAN"),("MEMBER", "MEMBER")])
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    status = models.BooleanField(choices=[("BORROWED", False), ("AVAILABLE", True)])
+
+    def __str__(self):
+        return self.name
+
+class Record(models.Model):
+    book = models.ForeignKey("Book", on_delete=models.CASCADE)
+    user = models.ForeignKey("MyUser", on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
